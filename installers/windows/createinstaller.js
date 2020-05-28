b3fc80c459079e6c212e79499869a505751a8f06
Jenelyn Contillo <jenelyn.contillo@hyperionworks.com>
2020-05-29 07:55:32 +0800

    Added installers

---
+++ installers/windows/createinstaller.js
@@ -0,0 +1,25 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+
+getInstallerConfig()
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.error(error.message || error)
+    process.exit(1)
+  })
+
+function getInstallerConfig () {
+  console.log('creating windows installer')
+  const rootPath = path.join('./')
+  const outPath = path.join(rootPath, 'release-builds')
+
+  return Promise.resolve({
+    appDirectory: path.join(outPath, 'pof-time-tracker-app-win32-ia32/'),
+    authors: 'Jenelyn Contillo',
+    noMsi: true,
+    outputDirectory: path.join(outPath, 'windows-installer'),
+    exe: 'pof-time-tracker-app.exe',
+    setupExe: 'POFTimeTrackerInstaller.exe',
+    setupIcon: path.join(rootPath, 'assets', 'icons', 'win', 'icon.ico')
+  })
+}
